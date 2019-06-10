6e383298e04fde286b3a434d9cb4b4ef95efed3c
mmurzin <goodlost@yandex.ru>
2019-06-10 22:08:56 +0300

    behaviour added

--- homeworks/homework-1/controllers/ProfileViewController.swift
+++ homeworks/homework-1/controllers/ProfileViewController.swift
@@ -12,7 +12,7 @@ class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        addBehaviors(behaviors: [ChangeColorBehavior()])
+        addBehaviors(behaviors: [ChangeColorBehavior(), HideNavigationBarBehavior()])
     }
 
 }
