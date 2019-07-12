c27c71e17e17389547280c80ec218a506eb4dd0a
mmurzin <goodlost@yandex.ru>
2019-07-12 14:48:53 +0300

    shared ui

--- homeworks/homework-1/shared/SharedViewController.swift
+++ homeworks/homework-1/shared/SharedViewController.swift
@@ -9,22 +9,72 @@
 import UIKit
 
 class SharedViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    
+//
+//    1. Реализовать для приложения ShareExtension
+//    1.1 Настроить чтобы с любого сайта можно было выделить текст и отправить в приложение
+//    1.2 При получениии ревеста от ShareExtension в приложении показывать ViewController
+//    2. В этом ViewController:
+//    2.1 Отображать Segmented Control переключения между локалями, например: английской(США), французской, китайской
+//    2.2 Под ним Label с пошаренным текстом
+//    2.3 Текст должен быть разобран существующие в нем даты и единицы измерений
+//    3. При переключении сегментов в тексте поменять в тексте даты и единицы измерения на локализованные
+//
+//    *4. Поставить сегмент-контролы(100, 1К, 10К, 100К, 1М, 10М) вместо слайдеров во вьюконтроллерах тестов структур данных
+//    Критерии оценки: Факт сдачи дз - 40 баллов
+//    Сдача во время - 10 баллов
+//    Наличие механизма шаринга - 20 баллов
+//    Переключение локалей на тексте - 30 баллов
+    
+    var sharedText = ""
+    let searchRegexes = [#"\d{1,2}\/\d{1,2}\/\d{4}"#]
+    var locales = [Locale]()
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var sharedTextVIew: UITextView!
+    @IBOutlet weak var localeSegments: UISegmentedControl!
+    
+    @IBAction func segmentedIndexChanged(_ sender: Any) {
+        loadCurrentTabData()
     }
-    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sharedTextVIew.text = sharedText
+        initLocales()
+        initLocaleSegmants()
+        loadCurrentTabData()
+    }
+    
+    func loadCurrentTabData() {
+        let currentLocale = locales[localeSegments.selectedSegmentIndex]
+        print("currentLocale \(currentLocale)")
+    }
+    
+    func initLocales() {
+        locales.append(Locale(identifier: "en"))
+        locales.append(Locale(identifier: "fr"))
+        locales.append(Locale(identifier: "zh_CN"))
+    }
+    
+    func initLocaleSegmants() {
+        localeSegments.removeAllSegments()
+        for locale in locales {
+            localeSegments.insertSegment(withTitle: locale.identifier, at: localeSegments.numberOfSegments, animated: false)
+        }
+        localeSegments.selectedSegmentIndex = 0
+    }
+    
+    func search(text: String, regex: String) -> [String] {
+        do {
+            let regex2 = try NSRegularExpression(pattern: regex)
+            let results = regex2.matches(in: text,
+                                         range: NSRange(text.startIndex..., in: text))
+            return results.map { String(text[Range($0.range, in: text)!]) }
+        }  catch let error {
+            print(error)
+        }
+        return [String]()
+    }
 
 }
